daeb98dd2b65f5f322f6a5b7c88150da7b697520
cih8086 <120590111@qq.com>
2018-08-05 06:42:54 +0000

    修改欢迎页文字

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello Wrod!!!!
+                    第一个Laravel程序
                 </div>
 
                 <div class="links">
